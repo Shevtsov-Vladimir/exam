f1ab01d27c293462d3418c5d9cf9c69a8b6b3513
Shevtsov-Vladimir <93482821+Shevtsov-Vladimir@users.noreply.github.com>
2022-12-23 02:19:21 +0300

    Add files via upload

---
+++ survey.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Дек 22 2022 г., 23:39
+-- Версия сервера: 10.4.27-MariaDB
+-- Версия PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `survey`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `answers`
+--
+
+CREATE TABLE `answers` (
+  `answer_id` int(10) UNSIGNED NOT NULL,
+  `q1` tinyint(3) UNSIGNED NOT NULL,
+  `q2` tinyint(3) UNSIGNED NOT NULL,
+  `q3` tinyint(3) UNSIGNED NOT NULL,
+  `comment` text DEFAULT NULL,
+  `explanation` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`answer_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `answer_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
